f041a2caa09f01188577bc3fa4f8aae6c385cc94
Tom Johnson <tom@tmjohnson.co.uk>
2021-01-22 19:36:06 +0000

    Add test for multiline comments

--- src/test/java/com/tmjohnson/jloc/ScannerTest.java
+++ src/test/java/com/tmjohnson/jloc/ScannerTest.java
@@ -4,12 +4,12 @@ import static com.tmjohnson.jloc.TokenType.CLASS;
 import static com.tmjohnson.jloc.TokenType.ELSE;
 import static com.tmjohnson.jloc.TokenType.EOF;
 import static com.tmjohnson.jloc.TokenType.IDENTIFIER;
+import static com.tmjohnson.jloc.TokenType.LEFT_PAREN;
 import static com.tmjohnson.jloc.TokenType.NUMBER;
 import static com.tmjohnson.jloc.TokenType.PLUS;
-import static org.junit.Assert.assertEquals;
+import static com.tmjohnson.jloc.TokenType.RIGHT_PAREN;
+import static org.junit.Assert.assertArrayEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,13 +17,24 @@ import org.junit.Test;
 public class ScannerTest {
     @Test
     public void canScanSimpleInput() {
-        List<Token> expected = new ArrayList<Token>(Arrays.asList(new Token(NUMBER, "1", 1.0, 1),
-                new Token(PLUS, "+", null, 1), new Token(CLASS, "class", null, 1), new Token(ELSE, "else", null, 1),
-                new Token(IDENTIFIER, "myname", null, 1), new Token(EOF, "", null, 1)));
+        Token[] expected = { new Token(NUMBER, "1", 1.0, 1), new Token(PLUS, "+", null, 1),
+                new Token(CLASS, "class", null, 1), new Token(ELSE, "else", null, 1),
+                new Token(IDENTIFIER, "myname", null, 1), new Token(EOF, "", null, 1) };
 
         String source = "1 + class else myname // comment";
         Scanner scanner = new Scanner(source);
         List<Token> output = scanner.scanTokens();
-        assertEquals(output, expected);
+        assertArrayEquals(output.toArray(), expected);
+    }
+
+    @Test
+    public void canScanMultilineInput() {
+        Token[] expected = { new Token(NUMBER, "1.23", 1.23, 1), new Token(LEFT_PAREN, "(", null, 4),
+                new Token(CLASS, "class", null, 4), new Token(RIGHT_PAREN, ")", null, 4), new Token(EOF, "", null, 4) };
+
+        String source = "1.23 /* multilinecomment \n lots of text \n on multiple lines \n*/ (class)";
+        Scanner scanner = new Scanner(source);
+        List<Token> output = scanner.scanTokens();
+        assertArrayEquals(output.toArray(), expected);
     }
 }
